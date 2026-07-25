5f668a48a58959e9f5fd2f3936e5f31f52d37ced
raj mishra <mrrajmishra573@gmail.com>
2026-07-25 09:53:30 +0530

    second coomit

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel 
+from typing import Optional
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"message": "Hello, World!"}
+@app.get("/blog")
+def index(limit: int =10 , published: bool = True, sort: Optional[str] = None):     
+    if published:
+        return {"data": f"Published blog list with limit {limit}" }
+    else:
+        return {"data": f"Unpublished blog list with limit {limit}" }
 
-@app.get("/about")
-def about():
-    return {"message": "This is about page."}    
+@app.get("/blog/{blog_id}")
+def show(blog_id: int):
+    return {"data": blog_id}    
 
 
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+    return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {"data":f"Blog is created with title as {request.title} and body as {request.body} and published status is {request.published}"}
+  
